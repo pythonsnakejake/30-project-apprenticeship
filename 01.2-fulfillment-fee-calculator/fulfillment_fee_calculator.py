2e21a1453698fabeb4814b732df87c1725def4fb
Jake Samuels <samuels242@googlemail.com>
2026-06-19 19:55:59 +0100

    Input Capture & Type Constraints

---
+++ 01.2-fulfillment-fee-calculator/fulfillment_fee_calculator.py
@@ -0,0 +1,15 @@
+def calculated_fulfillment_fee(order_id, item_count, priority_client):
+    if type(item_count) != int:
+        print("❌ ERROR: Item count must be a whole integer.")
+        return 
+    
+    if item_count <= 0:
+        print("❌ ERROR: Item count must be greater than 0.")
+        return
+
+    if item_count <= 5:
+        intial_price = 5.0
+    elif item_count <= 20:
+        intial_price = item_count * 4.50
+    else:
+        intial_price = item_count * 3.75
