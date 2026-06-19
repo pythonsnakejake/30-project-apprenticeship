c9e0e66bd926da4f704dc5a1ec40069692628d14
Jake Samuels <samuels242@googlemail.com>
2026-06-19 20:26:07 +0100

    Dynamic Fulfillment Fee Calculator

--- 01.2-fulfillment-fee-calculator/fulfillment_fee_calculator.py
+++ 01.2-fulfillment-fee-calculator/fulfillment_fee_calculator.py
@@ -8,16 +8,16 @@ def calculated_fulfillment_fee(order_id, item_count, priority_client):
         return
 
     if item_count <= 5:
-        intial_price = 5.0
+        initial_price = 5.0
     elif item_count <= 20:
-        intial_price = item_count * 4.50
+        initial_price = item_count * 4.50
     else:
-        intial_price = item_count * 3.75
+        initial_price = item_count * 3.75
 
     if priority_client:
-        intial_price *= 0.9
+        initial_price *= 0.9
 
-    final_price = intial_price + 1.5
+    final_price = initial_price + 1.5
 
     print(f"Total Price: £{final_price:.2f}")
 
