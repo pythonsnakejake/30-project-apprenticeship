d0f130b66df0116b7c8869e0db0bee3dd1bb999a
Jake Samuels <samuels242@googlemail.com>
2026-06-21 14:18:36 +0100

    Aggregate Reporting & Integration Testing

--- module-2/2.1-cvs-mainfest-parser/manifest_parser.py
+++ module-2/2.1-cvs-mainfest-parser/manifest_parser.py
@@ -1,11 +1,23 @@
 total_weight = 0.0
+record_count = 0
 
 file_name = "./shipping_manifest.csv"
 
 with open(file_name, "r") as file:
-    next(file)
+    next(file) # Skips first line of the .csv file
     
     for line in file:
         tracking_id, carrier, weight_raw = line.strip().split(",")
         
+        total_weight += float(weight_raw)
+        
+        record_count += 1
+        
         print(f"TRACKING ID: {tracking_id} | CARRIER: {carrier} | WEIGHT: {weight_raw}kg")
+        
+print(f"""
+========================================
+Total Rows Processed: {record_count}
+Total Manifest Weight: {total_weight}kg
+========================================
+""")
